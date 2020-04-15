11b767e2ddc66aae7c07b4759ff4a6b7a1cad499
CrestoniX <loltm.crav4enko@gmail.com>
2020-04-15 16:58:41 +0300

    finale

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Labs/lab-07-asio-server
+# Build directory: D:/Labs/lab-07-asio-server/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "D:/Labs/lab-07-asio-server/cmake-build-debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "D:/Labs/lab-07-asio-server/CMakeLists.txt;78;add_test;D:/Labs/lab-07-asio-server/CMakeLists.txt;0;")
